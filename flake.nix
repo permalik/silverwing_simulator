06cc544e23f4fd810a04214c2a803fa23684125b
permalik <permalik@protonmail.com>
2025-04-02 21:42:29 -0500

    feat: impl window

--- flake.nix
+++ flake.nix
@@ -13,24 +13,49 @@
   }:
     flake-utils.lib.eachDefaultSystem (
       system: let
-        pkgs = import nixpkgs {
-          inherit system;
-          config.allowUnfree = false;
-        };
+        name = "silverwing_simulator";
+        src = ./.;
+        pkgs = nixpkgs.legacyPackages.${system};
+        buildInputs = with pkgs; [SDL2 SDL2.dev];
+        nativeBuildInputs = with pkgs; [clang-tools];
       in {
-        devShells.default = pkgs.mkShell {
-          buildInputs = [
-            pkgs.alejandra
-            pkgs.gcc
-            pkgs.gnumake
-            pkgs.pkg-config
-            pkgs.llvmPackages_19.clang-tools
-            pkgs.SDL2
-            # pkgs.gdb
-            # pkgs.valgrind
-          ];
+        packages = {
+          default = let
+            inherit (pkgs) clangStdenv;
+          in
+            clangStdenv.mkDerivation {
+              name = "silverwing_simulator";
+              src = pkgs.lib.cleanSource ./.;
+              buildInputs = with pkgs; [SDL2];
 
-          shellHook = ''
+              buildPhase = with pkgs; ''
+                # export CXXFLAGS="-I${SDL2.dev}/include/SDL2":
+                clang++ ./src/silverwing_simulator.cpp -o silverwing_simulator -lSDL2
+              '';
+
+              installPhase = ''
+                mkdir -p $out/bin
+                cp silverwing_simulator $out/bin/silverwing_simulator
+              '';
+            };
+        };
+        devShells.default = pkgs.mkShell.override {stdenv = pkgs.clangStdenv;} {
+          packages = with pkgs; [pkg-config clang-tools SDL2.dev alejandra];
+
+          inputsFrom = [self.packages.${system}.default];
+          # buildInputs = [
+          #   pkgs.alejandra
+          #   pkgs.gcc
+          #   pkgs.gnumake
+          #   pkgs.pkg-config
+          #   pkgs.llvmPackages_19.clang-tools
+          #   pkgs.SDL2
+          #   # pkgs.gdb
+          #   # pkgs.valgrind
+          # ];
+
+          shellHook = with pkgs; ''
+            # CXXFLAGS="-I${pkgs.SDL2.dev}/include/SDL2"
             # Custom Prompt
             export PS1="\n\[\e[1;32m\][devshell](silverwing_simulator) \w\n❯ \[\e[0m\]"
           '';
--- src/silverwing_simulator.cpp
+++ src/silverwing_simulator.cpp
@@ -1,18 +1,84 @@
 #include <SDL2/SDL.h>
+#include <stdio.h>
+#include <cmath>
 
-int main(int arc, char ** argv) {
-    if (SDL_Init( SDL_INIT_VIDEO ) < 0) {
-        printf("SDL could not initialize!\n");
-    } else {
-        SDL_CreateWindow(
-            "SDL2 Demo",
-            SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-            800, 600,
-            SDL_WINDOW_SHOWN
-        );
+const int WINDOW_WIDTH = 800;
+const int WINDOW_HEIGHT = 600;
+const float PI = 3.1415926535f;
 
-        SDL_Delay(2000);
+void drawTriangle(SDL_Renderer *renderer, float angle) {
+    SDL_Point points[4];
+    float cx = WINDOW_WIDTH / 2.0f;
+    float cy = WINDOW_HEIGHT / 2.0f;
+    float size = 100.0f;
+
+    for (int i = 0; i < 3; i++) {
+        float theta = angle + (2 * PI * i / 3);
+        points[i].x = static_cast<int>(cx + size * cos(theta));
+        points[i].y = static_cast<int>(cy + size * sin(theta));
+    }
+    points[3] = points[0]; // Close the triangle
+
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    SDL_RenderDrawLines(renderer, points, 4);
+}
+
+int main(int argc, char **argv) {
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+        return 1;
     }
 
+    SDL_Window *window = SDL_CreateWindow("Silverwing Sim", SDL_WINDOWPOS_CENTERED,
+                                          SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH, 
+                                          WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
+    if (!window) {
+        printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (!renderer) {
+        printf("Renderer could not be created! SDL_Error: %s\n", SDL_GetError());
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    bool running = true;
+    SDL_Event event;
+    float angle = 0.0f;
+
+    while (running) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                running = false;
+            }
+        }
+
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderClear(renderer);
+
+        drawTriangle(renderer, angle);
+        SDL_RenderPresent(renderer);
+
+        angle += 0.02f; // Rotation speed
+        SDL_Delay(16); // Roughly 60 FPS
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
     return 0;
 }
+
+
+// #include <SDL2/SDL.h>
+// #include <stdio.h>
+//
+// int main(int arc, char **argv) {
+//   SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_INFORMATION, "Silverwing Sim",
+//                            "Starting SSS", 0);
+//   return 0;
+// }
